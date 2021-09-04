4c3469eb4479e120795c4ae342b174d209100d52
TryTryAchieve <sushantsb026@gmail.com>
2021-09-04 19:06:36 +0530

    style modification

--- hex.js
+++ hex.js
@@ -1,7 +1,7 @@
 const selec=['0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F']
 let btn=document.getElementById('btn')
 let text=document.querySelector('span')
-let bg=document.querySelector('main')
+let bg=document.body
 
 
 btn.addEventListener('click',function(){
